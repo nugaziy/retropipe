aa516b8e31a819b85c1755451e7383a5309e154d
Gaiaz <nugaziy@gmail.com>
2016-03-24 17:54:29 +0300

    added sam2table

---
+++ samfilterR.py
@@ -0,0 +1,63 @@
+from simplesam import Reader, Writer
+import sys, os, re
+from utils import *
+from os import listdir
+from os.path import isfile, join
+
+def sam2table(inputdir, in_sam_filename, outputdir, tablefilename, out_sam_errorfilename):
+    in_sam_file = open(inputdir + in_sam_filename, 'r')
+    tablefile = open(outputdir + tablefilename, 'w')
+    out_sam_errorfile = open(outputdir + out_sam_errorfilename, 'w')
+    in_sam = Reader(in_sam_file)
+    out_sam_error = Writer(out_sam_errorfile)
+    tablefile.write('ID\tCHR\tSTRAND\tSTART\tEND\tREAD1\tREAD2\tBARCODE\tALU\tCIGAR_R1\tCIGAR_R2\tMDFLAG_R1\tMDFLAG_R2\n')
+    count = 0
+    id_count = 0
+    for read in log_progress(in_sam, name = in_sam_filename, size = count_fastq_records(inputdir + in_sam_filename), every = 250):
+        count += 1
+        if count % 2 == 1:
+            read1 = read
+            if read1.paired and read1.mapped and not read1.duplicate:
+                read1_bool = True
+            else:
+                read1_bool = False
+        else:
+            read2 = read
+            if read1_bool:
+                if read2.paired and read2.mapped and not read2.duplicate:
+                    barcode = read2.qname.split('__ab:')[2]
+                    alu_seq = read1.qname.split('__ab:')[1]
+                    if read1.reverse:
+                        strand = '-'
+                    else:
+                        strand = '+'
+                    end_pos = read1.pos + read1.tlen - 1
+                    id_count += 1
+                    mdflag_r1 = read1._tags[1]
+                    mdflag_r2 = read2._tags[1]
+                    tablefile.write(str(int(id_count)) + '\t' + read1.rname + '\t' + 
+strand + '\t' + str(read1.pos) + '\t' + str(end_pos) + '\t' + 
+read1.seq + '\t' +read2.seq + '\t' + barcode + '\t' + alu_seq + '\t' + 
+read1.cigar + '\t' + read2.cigar + '\t' + mdflag_r1 + '\t' + mdflag_r2 + '\n')
+                else:
+                    out_sam_error.write(read1)
+                    out_sam_error.write(read2)
+    in_sam_file.close()
+    tablefile.close()
+    out_sam_errorfile.close()
+
+def main(inputdir, outputdir):
+    inputdir += "/"
+    outputdir += "/"
+    if not os.path.exists(outputdir):
+        os.makedirs(outputdir)
+    # Read files in folder
+    onlyfiles = [f for f in listdir(inputdir) if isfile(join(inputdir, f))]
+    for filename in onlyfiles:
+        filename = filename.rstrip()
+        inputfile, ext = os.path.splitext(filename)
+        if ext == '.sam':
+            in_sam_filename = filename
+            tablefilename = inputfile + '_table.txt'
+            out_sam_errorfilename = inputfile + '_error.sam'
+            sam2table(inputdir, in_sam_filename, outputdir, tablefilename, out_sam_errorfilename)
