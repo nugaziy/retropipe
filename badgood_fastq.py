a38661be5e13bb28d96baea8d415127567911d84
Gaiaz <nugaziy@gmail.com>
2016-04-11 13:10:42 +0300

    add intersection between megatable and libraries

--- badgood_fastq.py
+++ badgood_fastq.py
@@ -1,115 +1,27 @@
-#!usr/bin/python3
+# Import module (main code)
+import trimmR
+#import imp
+#imp.reload(trimmR)
 
-from Bio import SeqIO
-import sys, argparse, os
-import numpy as np
+# Variable parameters
+#################################
+# The number of permissible error:
+mist = 1
+# Primer, ad1 = Adapter 1, ad2 = Adapter 3 (aka Green)
+primer = 'GAGCCACCGCGC'
+ad1 = 'GCGTGCTGCGG'
+ad2 = 'AGGGCGGT'
+# Length of barcode
+barlen = 9
+# List of wrong elements in flank
+elem_remove = ['ACGT']
 
-parser = argparse.ArgumentParser(description='Trim fastq reads (r1, r2) by primer and adapters')
-parser.add_argument('-i', '--input', help='input files', nargs='+')
-parser.add_argument('-o', '--output', help='output directory')
-parser.add_argument('-m','--mistake', type = int, help='amount of mistake')
-parser.add_argument('-p','--primer', help='primer')
-parser.add_argument('-a','--ads', help='adapter and ad.green', nargs='+')
-parser.add_argument('-b','--barlen', type = int, help='barcode length')
-args = parser.parse_args()
+# Input FASTQ files folder path.
+inputdir = 'input/'
+# Output folder for processed FASTQ files.
+outputdir = 'output/'
+#################################
 
-dir1, filename1 = os.path.split(args.input[0])
-dir2, filename2 = os.path.split(args.input[1])
+# Main function
 
-outputfile1, outputfile1_ext = os.path.splitext(filename1)
-outputfile2, outputfile2_ext = os.path.splitext(filename2)
-
-mist = args.mistake
-primer = args.primer
-ad1 = args.ads[0]
-ad2 = args.ads[1]
-barlen = args.barlen
-
-
-goodr1 = open(args.output + outputfile1 + '_good.fastq', 'w')
-goodr2 = open(args.output + outputfile2 + '_good.fastq', 'w')
-badr1 = open(args.output + outputfile1 + '_bad.fastq', 'w')
-badr2 = open(args.output + outputfile2 + '_bad.fastq', 'w')
-
-
-
-
-def hamming (x1, x2, m):
-	j = 0
-	for i in range(len(x1)):
-		if x1[i] != x2[i]:
-			j += 1
-			if j > m:
-				return (False)
-	return (True)
-
-def trim_primers(record, primer, m):
-	badgood = {"good":False, "bad":np.array([0, 0, 0])}
-	len_primer = len(primer)
-	ham_prim = (hamming(primer, record[0:len_primer], m))or(hamming(primer, record[1:(len_primer+1)], m))
-	if ham_prim:
-		badgood['good'] = True
-	else:
-		badgood['bad'] = np.array([1, 0, 0])
-	return badgood
-
-def trim_ads(record, ad1, ad2, barlen, m):
-	badgood = {"good":False, "bad":np.array([0, 0, 0])}
-	len_ad1 = len(ad1)
-	len_ad2 = len(ad2)
-	seq1 = record[0:len_ad1]
-	seq1_shift = record[1:(len_ad1+1)]
-	seq2 = record[(len_ad1+barlen):(len_ad1+barlen+len_ad2)]
-	seq2_shift = record[(len_ad1+barlen+1):(len_ad1+barlen+len_ad2+1)]
-	ham_ad1 = (hamming(ad1, seq1, m))or(hamming(ad1, seq1_shift, m))
-	ham_ad2 = (hamming(ad2, seq2, m))or(hamming(ad1, seq2_shift, m))
-	if (ham_ad1)and(ham_ad2):
-		badgood['good'] = True
-	else:
-		if not((ham_ad1)or(ham_ad2)):
-			badgood['bad'] = np.array([0, 1, 1])
-		elif not(ham_ad1):
-			badgood['bad'] = np.array([0, 1, 0])
-		else:
-			badgood['bad'] = np.array([0, 0, 1])
-	return badgood
-
-
-
-original_R1_reads = SeqIO.parse(args.input[0], "fastq")
-original_R2_reads = SeqIO.parse(args.input[1], "fastq")
-
-count = np.array([0, 0, 0])
-
-for zipi in zip(original_R1_reads, original_R2_reads):
-	r1,r2 = zipi
-	fr1 = trim_primers(r1, primer, mist)
-	if fr1['good']:
-		fr2 = trim_ads(r2, ad1, ad2, barlen, mist)
-		if fr2['good']:
-			goodr1.write(str(r1.format('fastq')))
-			goodr2.write(str(r2.format('fastq')))
-		else:
-			badread = str(r1.format('fastq'))
-			badread = badread.split('\n')
-			badread[0] = badread[0] + ' m:' + ''.join(np.char.mod('%d', fr2['bad']))
-			badread = '\n'.join(badread)
-			badr1.write(badread)
-			badr2.write(str(r2.format('fastq')))
-			count = np.sum([count, fr2['bad']], axis=0)
-	else:
-		badread = str(r1.format('fastq'))
-		badread = badread.split('\n')
-		badread[0] = badread[0] + ' m:' + ''.join(np.char.mod('%d', fr1['bad']))
-		badread = '\n'.join(badread)
-		badr1.write(badread)
-		badr2.write(str(r2.format('fastq')))
-		count = np.sum([count, fr1['bad']], axis=0)
-
-
-goodr1.close()
-goodr2.close()
-badr1.close()
-badr2.close()
-
-print (count)
+trimmR.main(inputdir, outputdir, mist, primer, ad1, ad2, barlen, elem_remove)
---
+++ intersectionR.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import intervaltree as it
+import sys, os, re
+from os import listdir
+from os.path import isfile, join
+
+def main(inputtable, inputlibrary, outputdir, window):
+	inputlibrary += "/"
+	outputdir += "/"
+
+	if not os.path.exists(outputdir):
+		os.makedirs(outputdir)
+
+	onlyfiles = [f for f in listdir(inputlibrary) if isfile(join(inputlibrary, f))]
+	megatable = pd.read_table(inputtable)
+	megatable_id = list(megatable['MEGACLUSTER_ID'])
+
+	megatable_group = megatable.groupby(['CHR', 'STRAND'])
+	tree_dict = {}
+	for name, group in  megatable_group:
+		start_group = [x - window for x in list(group['POS'])]
+		end_group = [x + window for x in list(group['POS'])]
+		tree_dict[name[0] + name[1]] = it.IntervalTree(it.Interval(start, end, data)
+		 for start, end, data in zip(start_group, end_group, list(group['MEGACLUSTER_ID'])))
+
+	for filename in onlyfiles:
+		repcolumn = {x : 'NA' for x in megatable_id}
+		filename = filename.rstrip()
+		inputfile, ext = os.path.splitext(filename)
+		replib = pd.read_table(inputlibrary + filename)
+		replib_group = replib.groupby(['CHR', 'STRAND'])
+		for name, group in replib_group:
+			if name[0] + name[1] in tree_dict:
+				if name[0] == '+':
+					point = zip(list(group['START']), list(group['NAME']))
+				else:
+					point = zip(list(group['END']), list(group['NAME']))
+				for pos, repname in point:
+					finter = tree_dict[name[0] + name[1]][pos]
+					if len(finter) > 0:
+						for x in finter:
+							repcolumn[x.data] = repname
+		repcolumn = {inputfile : list(repcolumn.values())}
+		repcolumn_df = pd.DataFrame(repcolumn)
+		megatable = megatable.join(repcolumn_df)
+	table = open(outputdir + 'megatable_inter_lib.txt', 'w')
+	table.close()
+	megatable.to_csv(outputdir + 'megatable_inter_lib.txt', index=None, sep='\t', mode='a')
