9238aa6928335171cb754b0458e81247c3bf3916
Gaiaz <nugaziy@gmail.com>
2016-04-27 16:02:37 +0300

    count amount of bc with hamming 1

---
+++ bcR.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import re
+from collections import defaultdict
+from collections import Counter
+import sys, os, re
+from utils import *
+from os import listdir
+from os.path import isfile, join
+from scipy.stats import gaussian_kde
+import numpy as np
+
+def hamming (x1, x2):
+    j = 0
+    for i in range(len(x1)):
+        if x1[i] != x2[i]:
+            j += 1
+    if j > 1:
+    	return(0)
+    else:
+    	return (1)
+
+def which (rowsum):
+	for x in rowsum:
+		if x > 2:
+			return(1)
+	return(0)
+
+def main(inputtable):
+
+	megatable = pd.read_table(inputtable)
+
+	k = 0
+	k_1 = 0
+	for index, row in megatable.iterrows():
+		k += 1
+		barcode = row['BARCODE_LIST'].split(',')
+		hamming_table = pd.DataFrame(columns = barcode, index = barcode)
+		for i in range(len(barcode)):
+			for j in  range(len(barcode)):
+				hamming_table.set_value(barcode[i], barcode[j], hamming(barcode[i], barcode[j]))
+		rowsum = hamming_table.sum(axis = 1)
+		k_1 += which(rowsum)
+	print(k_1 / k)
